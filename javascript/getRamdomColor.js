a94f2759d6d1391f2df72f0acae21386e5b906fb
David Castillo <hola@davecastworks.com>
2021-01-30 08:22:37 -0400

    [fix] getRandomColor function name

--- javascript/getRamdomColor.js
+++ javascript/getRamdomColor.js
@@ -1,4 +1,4 @@
-function getRamdomColor() {
+function getRandomColor() {
   let color = Math.floor(Math.random() * 16777215).toString(16);
-  return color.length < 6 ? getRamdomColor() : color;
+  return color.length < 6 ? getRandomColor() : color;
 }
